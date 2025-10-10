8a267a9c00a2f85ecfbc3bd7fdbbbdfb3a2a8c7f
Hexun Zhang <200275zhx@gmail.com>
2025-10-10 15:38:50 -0400

    27

---
+++ 27. Remove Element/solution.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int write = 0;
+        for (int read = 0; read < nums.size(); ++read) {
+            if (nums[read] != val) {
+                nums[write++] = nums[read];
+            }
+        }
+        return write;
+    }
+};

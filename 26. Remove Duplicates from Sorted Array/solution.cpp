b03bb161f2ea2b166df1966fee523a8978312aa3
Hexun Zhang <200275zhx@gmail.com>
2025-10-12 17:07:22 -0400

    update 26

--- 26. Remove Duplicates from Sorted Array/solution.cpp
+++ 26. Remove Duplicates from Sorted Array/solution.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
-        int write = 0;
+        int write = 1;
         for (size_t read = 1; read < nums.size(); ++read) {
-            if (nums[write] != nums[read]) {
-                nums[++write] = nums[read];
+            if (nums[write-1] != nums[read]) {
+                nums[write++] = nums[read];
             }
         }
-        return write + 1;
+        return write;
     }
 };
